fd9f2ff6811c48477b7c8f2b8219364b8719b51f
Serj <oaokuznetcov@mail.ru>
2023-05-22 17:26:30 +0300

    fourth change

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import { Telegraf } from 'telegraf'
+import { message } from 'telegraf/filters'
 import config from 'config'
 
 const bot = new Telegraf(config.get('TELEGRAM_TOKEN'))
 
-
+bot.on(message('voice'), async ctx => {
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+    console.log(link.href)
+    await ctx.reply(JSON.stringify(link, null, 2))
+  } catch (e) {
+    console.log(`Error while voice message`, e.message)
+  }
+})
 
 bot.command('start', async (ctx) => {
   await ctx.reply(JSON.stringify(ctx.message, null, 2))
---
+++ src/ogg.js
@@ -0,0 +1,10 @@
+class OggConverter {
+  constructor() {}
+
+  toMp3() {}
+
+  create() {}
+
+}
+
+export const ogg = new OggConverter()
