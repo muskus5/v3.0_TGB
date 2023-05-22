8e75aec501bb03edf970bfcda1a7df6e2ce90e21
Serj <oaokuznetcov@mail.ru>
2023-05-22 18:31:44 +0300

    fifth change

--- src/ogg.js
+++ src/ogg.js
@@ -1,10 +1,32 @@
-class OggConverter {
-  constructor() {}
+import axios from "axios"
+import { createWriteStream } from 'fs'
+import { dirname, resolve } from 'path'
+import { fileURLToPath } from 'url'
 
-  toMp3() {}
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-  create() {}
+  class OggConverter {
+    constructor() { }
 
-}
+    toMp3() { }
+
+    async create(url, filename) {
+      try {
+        const oggPath = resolve(__dirname, '../voices' `${filename}.ogg`)
+        const response = await axios({
+          method: 'get',
+          url,
+          responseType: 'stream'
+        })
+        const stream = createWriteStream(oggPath)
+        response.data.pipe(stream)
+        stream.on('finish', () => {})
+      } catch (e) {
+        console.log('Error while creating ogg', e.message)
+      }
+
+    }
+
+  }
 
 export const ogg = new OggConverter()
