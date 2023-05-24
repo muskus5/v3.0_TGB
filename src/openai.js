d7b338dcc13d5b471db801e5de4ae070d03b43aa
Serj <email>
2023-05-24 18:09:41 +0300

    seventh change

--- src/openai.js
+++ src/openai.js
@@ -3,6 +3,12 @@ import config from 'config'
 import { createReadStream } from 'fs'
 
 class OpenAI {
+roles = {
+  ASSISTANT: 'assistant',
+  USER: 'user',
+  SYSTEM: 'system',
+}
+
   constructor(apiKey) {
     const configuration = new Configuration({
       apiKey
@@ -10,7 +16,17 @@ class OpenAI {
     this.openai = new OpenAIApi(configuration);
   }
 
-  chat() { }
+  async chat(messages) { 
+    try {
+      const response = await this.openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
+      })
+      return response.data.choices[0].message
+    } catch (e) {
+      console.log('Error while gpt chat', e.message)
+    }
+  }
 
   async transcription(filepath) {
     try {
